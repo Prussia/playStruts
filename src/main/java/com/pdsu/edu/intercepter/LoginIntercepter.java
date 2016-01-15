3104b87f335e75bc67ff0158bac4b35a495b888e
Prussia <prussia.hu@gmail.com>
2016-01-15 21:20:46 +0800

    check session in loginIntercepter and redirect to login page

--- src/main/java/com/pdsu/edu/intercepter/LoginIntercepter.java
+++ src/main/java/com/pdsu/edu/intercepter/LoginIntercepter.java
@@ -5,21 +5,36 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.opensymphony.xwork2.Action;
+import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionInvocation;
 import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
+import com.pdsu.edu.domain.User;
 
 public class LoginIntercepter extends AbstractInterceptor {
-	
+
 	private static final Logger log = LoggerFactory.getLogger(LoginIntercepter.class);
 
 	@Override
 	public String intercept(ActionInvocation invocation) throws Exception {
-		Map<String, Object> parameters = invocation.getInvocationContext().getParameters();
-		System.out.println(parameters.toString());
-		log.info("user.username = " + parameters.get("user.username"));
-//		return Action.INPUT;
+		String namespace = invocation.getProxy().getNamespace();  
+        String actionName = invocation.getProxy().getActionName();  
+          
+        if(
+        		("/user".equals(namespace) && "user_login".equals(actionName))
+        		) {  
+            return invocation.invoke();  
+        } else {  
+            Map<String, Object> session = ActionContext.getContext().getSession();  
+            User user = (User) session.get("cur_user");  
+            if(user == null) {  
+                return Action.LOGIN;  
+            } else {  
+                return invocation.invoke();  
+            }  
+              
+        }  
 
-		return invocation.invoke();
 	}
 
 }
